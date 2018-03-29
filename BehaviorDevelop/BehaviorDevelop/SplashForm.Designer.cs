4f35ecbc9d6c1f5c55423461ef8c407c4fd5450d
hiroshi.tsuchiya <tuch_ctc@anasystems.co.jp>
2018-03-29 18:38:24 +0900

    ローカルDBの作成処理は別プロセスで行うが、ここに数十秒程度の時間がかかる場合があるため、BehaviorDevelop側の画面をモーダルで停止させ、前に進めなくするためのフォームを追加。 ※起動時・プロジェクトファイルのオープン時にローカルDBを新規に作成することがあるが、これを別プロセスのCUIアプリケーションで処理しているために画面側での待ち状態を意図的に作っている

---
+++ BehaviorDevelop/BehaviorDevelop/SplashForm.Designer.cs
@@ -0,0 +1,78 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: ctc0065
+ * Date: 2018/01/26
+ * Time: 13:50
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+namespace BehaviorDevelop
+{
+	partial class SplashForm
+	{
+		/// <summary>
+		/// Designer variable used to keep track of non-visual components.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+		
+		/// <summary>
+		/// Disposes resources used by the form.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing) {
+				if (components != null) {
+					components.Dispose();
+				}
+			}
+			base.Dispose(disposing);
+		}
+		
+		/// <summary>
+		/// This method is required for Windows Forms designer support.
+		/// Do not change the method contents inside the source code editor. The Forms designer might
+		/// not be able to load this method if it was changed manually.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.label1 = new System.Windows.Forms.Label();
+			this.label2 = new System.Windows.Forms.Label();
+			this.SuspendLayout();
+			// 
+			// label1
+			// 
+			this.label1.Location = new System.Drawing.Point(21, 26);
+			this.label1.Name = "label1";
+			this.label1.Size = new System.Drawing.Size(246, 18);
+			this.label1.TabIndex = 0;
+			this.label1.Text = "内部DBを作成中です。しばらくお待ちください";
+			// 
+			// label2
+			// 
+			this.label2.Location = new System.Drawing.Point(21, 44);
+			this.label2.Name = "label2";
+			this.label2.Size = new System.Drawing.Size(232, 23);
+			this.label2.TabIndex = 1;
+			this.label2.Text = "（処理終了後、自動的に閉じます）";
+			// 
+			// SplashForm
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(279, 79);
+			this.ControlBox = false;
+			this.Controls.Add(this.label2);
+			this.Controls.Add(this.label1);
+			this.MaximizeBox = false;
+			this.MinimizeBox = false;
+			this.Name = "SplashForm";
+			this.ShowInTaskbar = false;
+			this.Text = "SplashForm";
+			this.Load += new System.EventHandler(this.SplashFormLoad);
+			this.ResumeLayout(false);
+		}
+		private System.Windows.Forms.Label label2;
+		private System.Windows.Forms.Label label1;
+	}
+}
---
+++ BehaviorDevelop/BehaviorDevelop/SplashForm.cs
@@ -0,0 +1,63 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: ctc0065
+ * Date: 2018/01/26
+ * Time: 13:50
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+using System;
+using System.Drawing;
+using System.Diagnostics;
+using System.Windows.Forms;
+
+namespace BehaviorDevelop
+{
+	/// <summary>
+	/// Description of SplashForm.
+	/// </summary>
+	public partial class SplashForm : Form
+	{
+		Boolean loaded;
+		
+		public SplashForm()
+		{
+			//
+			// The InitializeComponent() call is required for Windows Forms designer support.
+			//
+			InitializeComponent();
+			loaded = false;
+		}
+
+		void SplashFormLoad(object sender, EventArgs e)
+		{
+			loaded = true; 
+		}
+		
+		public void CloseOnLoadComplete(string projectPath, string dbfile) {
+			
+			if (!this.loaded) {
+				Int32 count;
+				for(count=0; count < 1000; count++ ) {
+					if( this.loaded ) {
+						break;
+					} else {
+						System.Threading.Thread.Sleep(100);
+					}
+				}
+			}
+			
+			// このプログラム(.exe)が置かれたフォルダを取得
+			string currentDir = System.Windows.Forms.Application.StartupPath ;
+
+			//ファイルを開いて終わるまで待機する
+			Process p = Process.Start( currentDir + "\\ElementIndexer.exe", projectPath + " " + dbfile );
+//					MessageBox.Show("内部データベースを構築します。\n構築処理が完了後、OKボタンを押してください");
+			
+			// サブプロセスが終了後、必要なくなったところでCLOSE
+			p.WaitForExit();
+			
+			this.Close();
+		}
+	}
+}
