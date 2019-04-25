30b9469153cb4626ac95b747c16d8c3cc62074de
hiroshi.tsuchiya <hiroshi.tsuchiya@ctc-g.co.jp>
2019-04-25 10:21:51 +0900

    リファクタリング、機能追加 　・要素の内容をAsciiDoc出力するにあたって、ElementAsciidocWriter と ArtifactAsciidocWriter とで処理が重複していた（しかもArtifactの方が先行していた）ので、 　　ElementAsciidocWriterの方に処理を集約した。 　・ふるまいの出力にあたって、ふるまいをそのまま出すのではなく、解析した結果を出すように変更

--- EaArtifactTool/AsciidocGenerator/ElementAsciidocWriter.cs
+++ EaArtifactTool/AsciidocGenerator/ElementAsciidocWriter.cs
@@ -1,5 +1,6 @@
 ﻿using ArtifactFileAccessor.util;
 using ArtifactFileAccessor.vo;
+using IndexAccessor;
 using System;
 using System.Collections.Generic;
 using System.IO;
@@ -10,6 +11,11 @@ namespace AsciidocGenerator
 {
     public class ElementAsciidocWriter
     {
+        /// <summary>
+        /// 要素毎のAsciidoc出力
+        /// </summary>
+        /// <param name="outputDir"></param>
+        /// <param name="elem"></param>
         public static void doWrite(string outputDir, ElementVO elem)
         {
             string filepath = outputDir + "\\elements\\" + elem.guid.Substring(1, 1) + "\\"
@@ -46,24 +52,25 @@ namespace AsciidocGenerator
 
 
         /// <summary>
-        /// 要素の出力
+        /// 要素のAsciidoc出力(既に開いているストリームに出力)
+        /// 成果物単位やパッケージ単位でのAsciidoc出力に対応
         /// </summary>
         /// <param name="element"></param>
         /// <param name="sw"></param>
-        private static void writeElement(ElementVO element, StreamWriter sw)
+        public static void writeElement(ElementVO element, StreamWriter sw)
         {
-
+            sw.WriteLine("//##BEGIN element GUID=" + element.guid + "##");
             string stereotypeStr = "";
             if (element.stereoType != null && element.stereoType != "")
             {
-                // stereotypeStr = "&#x00AB;" + element.stereoType + "&#x00BB;";
+                //                stereotypeStr = "&#x00AB;" + element.stereoType + "&#x00BB;";
                 stereotypeStr = "≪" + element.stereoType + "≫";
             }
 
-            sw.WriteLine("### 要素" + " (" + element.eaType + ") " + stereotypeStr + element.name);
+            // sw.WriteLine("### 要素" + " (" + element.eaType + ") " + stereotypeStr + element.name );
+            sw.WriteLine("### 要素:" + element.name);
 
             //sw.WriteLine("[horizontal]");
-            sw.WriteLine("GUID:: " + element.guid);
             sw.WriteLine("クラス宣言:: " + element.visibility + " " + element.eaType + " " + stereotypeStr + element.name + "[" + element.alias + "]");
             //sw.WriteLine("ステレオタイプ: " + element.stereoType);
             sw.WriteLine("");
@@ -104,11 +111,12 @@ namespace AsciidocGenerator
                     writeMethod(mth, sw);
                 }
             }
-
+            sw.WriteLine("//##END element" + "##");
         }
 
         private static void writeTaggedValueElem(List<TaggedValueVO> elemTags, StreamWriter sw)
         {
+            sw.WriteLine("//##BEGIN element-taggedvalues##");
             sw.WriteLine(".要素のタグ付き値");
             sw.WriteLine("[cols = \"1,3\"]");
             sw.WriteLine("|===");
@@ -116,6 +124,7 @@ namespace AsciidocGenerator
 
             foreach (TaggedValueVO tv in elemTags)
             {
+                sw.WriteLine("//##BEGIN taggedvalue GUID=" + tv.guid + "##");
                 sw.Write("|" + tv.name);
 
                 if (tv.tagValue == "<memo>")
@@ -128,14 +137,18 @@ namespace AsciidocGenerator
                 }
 
                 sw.WriteLine("");
+                sw.WriteLine("//##END taggedvalue##");
             }
             sw.WriteLine("|===");
             sw.WriteLine("");
+            sw.WriteLine("//##END element-taggedvalues##");
         }
 
 
         private static void writeAttribute(AttributeVO attribute, StreamWriter sw)
         {
+            sw.WriteLine("//##BEGIN attribute GUID=" + attribute.guid + "##");
+
             string stereotypeStr = "", staticFinalStr, defaultStr;
             if (attribute.stereoType != null && attribute.stereoType != "")
             {
@@ -145,7 +158,7 @@ namespace AsciidocGenerator
             // sw.WriteLine("#### 属性 " + stereotypeStr + attribute.name + " [" + attribute.alias + "]");
 
             sw.WriteLine("#### 属性 " + attribute.name + " [" + attribute.alias + "]");
-            sw.WriteLine("+GUID: " + attribute.guid + "+");
+            sw.WriteLine("// +GUID: " + attribute.guid + "+");
             sw.WriteLine("");
             sw.WriteLine("##### 宣言");
             sw.WriteLine("");
@@ -192,6 +205,7 @@ namespace AsciidocGenerator
             }
 
             sw.WriteLine("");
+            sw.WriteLine("//##END attribute##");
         }
 
         private static void writeTaggedValueAttr(List<TaggedValueVO> attributeTags, StreamWriter sw)
@@ -223,6 +237,8 @@ namespace AsciidocGenerator
 
         private static void writeMethod(MethodVO method, StreamWriter sw)
         {
+            sw.WriteLine("//##BEGIN method GUID=" + method.guid + "##");
+
             string stereotypeStr = "";
             if (method.stereoType != null && method.stereoType != "")
             {
@@ -231,12 +247,11 @@ namespace AsciidocGenerator
             }
 
             sw.WriteLine("#### 操作 " + stereotypeStr + method.name + "[" + method.alias + "]");
-            sw.WriteLine("+GUID: " + method.guid + "+");
+            sw.WriteLine("// +GUID: " + method.guid + "+");
             sw.WriteLine("");
             sw.WriteLine("##### 操作宣言");
             sw.WriteLine("");
-            sw.WriteLine("  " + method.visibility + " " + method.returnType + " " + stereotypeStr + method.name + " ()");
-            // sw.WriteLine("ステレオタイプ: " + method.stereoType);
+            sw.WriteLine("  " + method.visibility + " " + method.returnType + " " + stereotypeStr + method.name + " (" + method.getParamDesc() + ")");
             sw.WriteLine("");
 
             // パラメータの出力
@@ -248,7 +263,7 @@ namespace AsciidocGenerator
             // ノートの出力
             if (method.notes != null && method.notes != "")
             {
-                sw.WriteLine("#####  概要");
+                sw.WriteLine("#####  メソッドの概要");
                 sw.WriteLine("");
                 sw.WriteLine("[%hardbreaks]");
                 sw.WriteLine(method.notes);
@@ -268,14 +283,66 @@ namespace AsciidocGenerator
             {
                 sw.WriteLine("##### ふるまい");
                 sw.WriteLine("");
-                // sw.WriteLine(".ふるまい");
                 sw.WriteLine("------");
-                sw.WriteLine(method.behavior);
+                sw.WriteLine(getParsedBehavior(method));
                 sw.WriteLine("------");
 
                 sw.WriteLine("");
             }
+
+            sw.WriteLine("//##END method##");
         }
+
+
+        private static string getParsedBehavior(MethodVO mth)
+        {
+            BehaviorParser parser = new BehaviorParser();
+            List<BehaviorChunk> chunkList = parser.parseBehavior(mth);
+
+            StringWriter outsw = new StringWriter();
+
+            foreach (var bc in chunkList)
+            {
+                outsw.WriteLine(generateIndentStr(bc.indLv) + bc.behavior);
+            }
+
+            return outsw.ToString();
+        }
+
+
+        private static string generateIndentStr(int indentLevel)
+        {
+            switch( indentLevel )
+            {
+                case 0:
+                    return "";
+                case 1:
+                    return "# ";
+                case 2:
+                    return "## ";
+                case 3:
+                    return "### ";
+                case 4:
+                    return "#### ";
+                case 5:
+                    return "##### ";
+                case 6:
+                    return "###### ";
+                case 7:
+                    return "####### ";
+                case 8:
+                    return "######## ";
+                case 9:
+                    return "######### ";
+                case 10:
+                    return "########## ";
+                default:
+                    return "##########" + indentLevel + " ";
+            }
+
+        }
+
+
 
         private static void writeParameters(List<ParameterVO> parameters, StreamWriter sw)
         {
@@ -330,17 +397,18 @@ namespace AsciidocGenerator
         private static void writePlantUml(ElementVO element, StreamWriter sw)
         {
             sw.WriteLine("");
-            sw.WriteLine("[plantuml, images/" + element.guid.Substring(1,8) + ", svg]");
-            // sw.WriteLine("[plantuml, images/" + element.guid.Substring(1, 8) + ", png]");
+            // sw.WriteLine("[plantuml, images/" + element.guid.Substring(1,8) + ", svg]");
+            sw.WriteLine("[plantuml, img-" + element.guid.Substring(1, 13) + ", png]");
             sw.WriteLine("--");
             sw.WriteLine("@startuml");
-            sw.WriteLine("class " + element.name + " {");
+
+            sw.WriteLine("class \"" + filterSpecialChar(element.name) + "\" " + getStereotypeStr(element.stereoType) + " {");
 
             if (element.attributes != null)
             {
                 foreach (AttributeVO attr in element.attributes)
                 {
-                    sw.WriteLine("  " + getVisibilitySymbol(attr.visibility) + " " + attr.name);
+                    sw.WriteLine("  " + getVisibilitySymbol(attr.visibility) + " " + attr.name + "");
                 }
             }
 
@@ -353,9 +421,162 @@ namespace AsciidocGenerator
             }
 
             sw.WriteLine("}");
+
+            ElementSearcher elemSrch = new ElementSearcher();
+
+            // DBからこの要素につながっている接続情報を取得
+            ConnectorSearcher searcher = new ConnectorSearcher();
+            List<ConnectorVO> conns = searcher.findByObjectGuid(element.guid);
+
+            Dictionary<string, ElementSearchItem> nameHash = new Dictionary<string, ElementSearchItem>();
+
+            List<ConnectorVO> srcConnList = new List<ConnectorVO>();
+            List<ConnectorVO> destConnList = new List<ConnectorVO>();
+            foreach (ConnectorVO cn in conns)
+            {
+                if (cn.srcObjGuid == element.guid)
+                {
+                    srcConnList.Add(cn);
+                    outputConnDestClass(cn.destObjGuid, elemSrch, nameHash, sw);
+                }
+
+                if (cn.destObjGuid == element.guid)
+                {
+                    destConnList.Add(cn);
+                    outputConnDestClass(cn.srcObjGuid, elemSrch, nameHash, sw);
+                }
+            }
+
+
+            for (var i = 0; i < srcConnList.Count; i++)
+            {
+                outputSrcConnectLine(srcConnList[i], sw);
+            }
+
+            for (var i = 0; i < destConnList.Count; i++)
+            {
+                outputDestConnectLine(destConnList[i], sw);
+            }
+
             sw.WriteLine("@enduml");
             sw.WriteLine("--");
             sw.WriteLine("");
+        }
+
+
+        private static void outputConnDestClass(string objGuid, ElementSearcher elemSrch, Dictionary<string, ElementSearchItem> nameHash, StreamWriter sw)
+        {
+            ElementSearchItem elemSrcItem = elemSrch.findByGuid(objGuid);
+            if (elemSrcItem != null)
+            {
+                string identifiedName = filterSpecialChar(elemSrcItem.elemName);
+                if (!nameHash.ContainsKey(identifiedName))
+                {
+                    sw.WriteLine("class \"" + identifiedName + "\" " + getStereotypeStr(elemSrcItem.elemStereotype) + " {");
+                    sw.WriteLine("}");
+                    nameHash.Add(identifiedName, elemSrcItem);
+                }
+            }
+        }
+
+
+        private static void outputSrcConnectLine(ConnectorVO cn, StreamWriter sw)
+        {
+            string leftSideName, rightSideName, lineStyle;
+
+            leftSideName = cn.srcObjName;
+            rightSideName = cn.destObjName;
+
+            switch (cn.connectorType)
+            {
+                case "Association":
+                    lineStyle = " --> ";
+                    break;
+                case "Dependency":
+                    lineStyle = " ..> ";
+                    break;
+                case "Generalization":
+                    leftSideName = cn.destObjName;
+                    rightSideName = cn.srcObjName;
+                    lineStyle = " <|-- ";
+                    break;
+                case "Aggregation":
+                    leftSideName = cn.destObjName;
+                    rightSideName = cn.srcObjName;
+                    lineStyle = " o-- ";
+                    break;
+                case "Realisation":
+                    leftSideName = cn.destObjName;
+                    rightSideName = cn.srcObjName;
+                    lineStyle = " <|.. ";
+                    break;
+                case "NoteLink": // ノートリンクは出力の価値が無いので出力しないでリターン
+                    return;
+
+                default:
+                    lineStyle = " --- ";
+                    break;
+            }
+
+            sw.WriteLine("\"" + filterSpecialChar(leftSideName) + "\" " + lineStyle + "\"" + filterSpecialChar(rightSideName) + "\" ");
+        }
+
+
+        private static void outputDestConnectLine(ConnectorVO cn, StreamWriter sw)
+        {
+            string leftSideName, rightSideName, lineStyle;
+
+            leftSideName = cn.srcObjName;
+            rightSideName = cn.destObjName;
+
+            switch (cn.connectorType)
+            {
+                case "Association":
+                    lineStyle = " --> ";
+                    break;
+                case "Dependency":
+                    lineStyle = " ..> ";
+                    break;
+                case "Generalization":
+                    leftSideName = cn.destObjName;
+                    rightSideName = cn.srcObjName;
+                    lineStyle = " <|-- ";
+                    break;
+                case "Aggregation":
+                    leftSideName = cn.destObjName;
+                    rightSideName = cn.srcObjName;
+                    lineStyle = " o-- ";
+                    break;
+                case "Realisation":
+                    leftSideName = cn.destObjName;
+                    rightSideName = cn.srcObjName;
+                    lineStyle = " <|.. ";
+                    break;
+                case "NoteLink": // ノートリンクは出力の価値が無いので出力しないでリターン
+                    return;
+
+                default:
+                    lineStyle = " --- ";
+                    break;
+            }
+
+            sw.WriteLine("\"" + filterSpecialChar(leftSideName) + "\" " + lineStyle + "\"" + filterSpecialChar(rightSideName) + "\" ");
+        }
+
+
+        private static string filterSpecialChar(string orig)
+        {
+            System.Text.StringBuilder sb = new System.Text.StringBuilder(orig);
+            sb.Replace("(", "（");
+            sb.Replace(")", "）");
+            // sb.Replace("（", "〈");
+            // sb.Replace("）", "〉");
+            sb.Replace(" ", "_");
+            // sb.Replace("-", "_");
+            sb.Replace("^", "＾");
+            sb.Replace("？", "");
+
+            return sb.ToString();
         }
 
 
@@ -369,5 +590,19 @@ namespace AsciidocGenerator
                 default: return " ";
             }
         }
+
+
+        private static string getStereotypeStr(string stereoType)
+        {
+            if (stereoType != null && stereoType != "")
+            {
+                return " << " + stereoType + " >> ";
+            }
+            else
+            {
+                return "";
+            }
+        }
+
     }
 }
