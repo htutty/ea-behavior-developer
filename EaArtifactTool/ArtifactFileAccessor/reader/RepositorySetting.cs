7ebeaed1f4c954a14adc01ba9b9f78105c4b1b49
hiroshi.tsuchiya <hiroshi.tsuchiya@ctc-g.co.jp>
2019-12-17 18:37:08 +0900

    機能追加 　・リポジトリ名ベースでアクセスするDBが決まらないのが面倒だったので、リポジトリ名(例えば cuvic_aswea_master)と 　　接続文字列とプロジェクトファイル(.bdprj)などを紐づけて管理するためのXMLファイル(repositories.xml)を定義し、 　　それを読み取るための機能をリリース

---
+++ EaArtifactTool/ArtifactFileAccessor/reader/RepositorySetting.cs
@@ -0,0 +1,90 @@
+﻿using System.Collections.Generic;
+using System.Xml;
+using ArtifactFileAccessor.vo;
+using ArtifactFileAccessor.util;
+
+namespace ArtifactFileAccessor.reader
+{
+	/// <summary>
+	/// Description of ProjectSetting.
+	/// </summary>
+	public class RepositorySetting
+	{
+
+		public RepositorySetting()
+		{
+		}
+
+		/// <summary>
+        /// repositories.xml ファイルを読み、その内容をRepositorySettingVOのリストにセットして返却する
+        /// </summary>
+        /// <returns>bool: 読み込み成功ならtrue</returns>
+		public static List<RepositorySettingVO> load(string basePath) {
+            string repositoriesFilename = "repositories.xml";
+            string repositoryFile = basePath + "\\" + repositoriesFilename;
+
+            // XMLテキストをロードする
+            XmlDocument xmlDoc = new XmlDocument();
+            xmlDoc.Load(repositoryFile);
+
+            // 返却用のRepositorySettingVOリストを生成
+            List<RepositorySettingVO> repositorySettings = new List<RepositorySettingVO>();
+
+            // projectノードに移動する
+            XmlNode repositoriesNode = xmlDoc.SelectSingleNode("/repositories");
+            if ( repositoriesNode != null ) {
+	            foreach (XmlNode repoNode in repositoriesNode.ChildNodes) {
+                    if( repoNode.Name == "Repository" )
+                    {
+                        RepositorySettingVO settingvo = readRepositoryNode(repoNode);
+                        settingvo.projectSettingVO = ProjectSetting.readProjectSetting(settingvo.projectPath);
+                        repositorySettings.Add(settingvo);
+                    }
+	            }
+            }
+
+            return repositorySettings;
+        }
+
+
+        private static RepositorySettingVO readRepositoryNode(XmlNode repoNode)
+        {
+
+            RepositorySettingVO settingvo = new RepositorySettingVO();
+
+            foreach (XmlNode riNode in repoNode.ChildNodes)
+            {
+                switch (riNode.Name)
+                {
+                    case "repositoryName":
+                        settingvo.name = riNode.InnerText;
+                        break;
+
+                    case "baseRepository":
+                        settingvo.baseRepository = riNode.InnerText;
+                        break;
+
+                    case "localPath":
+                        settingvo.localPath = riNode.InnerText;
+                        break;
+
+                    case "projectPath":
+                        settingvo.projectPath = riNode.InnerText;
+                        break;
+
+                    case "connectionString":
+                        settingvo.connectionString = riNode.InnerText;
+                        break;
+
+                    default:
+                        // do nothing
+                        break;
+                }
+            }
+
+            return settingvo;
+        }
+
+    }
+
+}
---
+++ EaArtifactTool/ArtifactFileAccessor/vo/RepositorySettingVO.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ArtifactFileAccessor.vo
+{
+    public class RepositorySettingVO
+    {
+        public string name { get; set; }
+        public string baseRepository { get; set; }
+        public string localPath { get; set; }
+        public string projectPath { get; set; }
+        public string connectionString { get; set; }
+        public ProjectSettingVO projectSettingVO { get; set; }
+    }
+}
